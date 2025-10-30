6467fede6013ca02dda336e2a55e8eb5b9cfe751
Jwseph <116055108+jwsephdev@users.noreply.github.com>
2025-10-30 17:41:30 +0800

    Delete firstpersoncontroller directory

--- firstpersoncontroller/Scripts/sway.gd
+++
@@ -1,6 +0,0 @@
-extends Node3D
-@onready var gunpos: Node3D = $"../Head/neck/Camera/gunpos"
-var gunsway = 50.0
-
-func _process(delta: float) -> void:
-	global_transform = global_transform.interpolate_with(gunpos.global_transform, 40  * delta)
